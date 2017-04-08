b6e76b4773a2ad3232a59fc4a480cc0898d723a1
d.bitter <d.bitter@viriciti.nl>
2017-04-08 16:46:21 +0200

    refactoring

--- assets/js/je_bent_zelf_een.js
+++ assets/js/je_bent_zelf_een.js
@@ -1,59 +1,91 @@
-var elements = {
-	h1: document.getElementById('je_bent_zelf_een_h1'),
-	input: document.getElementById('je_bent_zelf_een_input')
-}
-
-elements.input.value = window.location.href
-elements.input.placeholder = window.location.href
-
-buildInsult(getUrlValues(window.location.href))
-
-var audio = new Audio('assets/audio/bdt.mp3');
-audio.play();
-
-
-function getUrlValues() {
-	var objects = []
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-	for (var i = 0; i < hashes.length; i++) {
-		var val = ''
-		if (typeof hashes[i].split('=')[1] !== 'undefined') {
-			val = hashes[i].split('=')[1].replace(/%20/g, " ");
-		} else {
-			val = "woord"
-			elements.input.value = window.location.href + '?q=woord'
+(function() {
+	var app = {
+		elements: {
+			h1: document.getElementById('je_bent_zelf_een_h1'),
+			input: document.getElementById('je_bent_zelf_een_input')
+		},
+		media: {
+			bdt: new Audio('assets/audio/bdt.mp3')
+		},
+		values: [],
+		url: window.location.href,
+		listen: function() {
+			submitEvent(this.elements.input)
+		},
+		init: function() {
+			app.listen()
+			replaceInputValues(this.elements.input)
+			this.values = getUrlValues(this.url)
+			buildInsult(this.values, this.elements.h1)
+			playAudio(this.media.bdt)
 		}
-		obj = {
-			value: val
+	}
+
+	function playAudio(audio) {
+		audio.play()
+	}
+
+	function getUrlValues() {
+		var objects = []
+		var hashes = app.url.slice(app.url.indexOf('?') + 1).split('&')
+		console.log(hashes)
+		for (var i = 0; i < hashes.length; i++) {
+			var val = ''
+			if (typeof hashes[i].split('=')[1] !== 'undefined') {
+				val = iNeedSpace(hashes[i].split('=')[1])
+			} else {
+				val = "woord"
+				app.elements.input.value = app.url + '?q=woord'
+			}
+			obj = {
+				value: val
+			}
+
+			objects.push(obj)
 		}
-
-		objects.push(obj)
+		return objects;
 	}
-	return objects;
-}
 
-function buildInsult(values) {
-	var message = ''
+	function buildInsult(values, h1) {
+		var message = ''
 
-	for (var i = 0; i < values.length; i++) {
-		if (i == 0) {
-			message += 'Je bent zelf een '
-		} else if (i == values.length - 1) {
-			message += ' en een '
-		} else if (i !== values.length) {
-			message += ", een "
-		}
+		values.forEach(function(value, i) {
+			switch (i) {
+				case i = 0:
+					message += 'Je bent zelf een '
+					message += value.value
+					break
+				case i = values.length - 1:
+					message += ' en een '
+					message += value.value
+					break
+				default:
+					message += ", een "
+					message += value.value
+			}
+		})
 
-		message += values[i].value
+		message += '!'
+
+		h1.innerHTML += message
 	}
-	message += '!'
 
-	elements.h1.innerHTML += message
-}
-
-elements.input.addEventListener("keyup", function(e) {
-	e.preventDefault();
-	if (e.keyCode == 13) {
-		window.location = e.target.value
+	function replaceInputValues(input) {
+		input.value = app.url
+		input.placeholder = app.url
 	}
-})
+
+	function iNeedSpace(string) {
+		return string.replace(/%20/g, " ")
+	}
+
+	function submitEvent(input) {
+		input.addEventListener("keyup", function(e) {
+			e.preventDefault()
+			if (e.keyCode == 13) {
+				window.location = e.target.value
+			}
+		})
+	}
+	app.init()
+})()
